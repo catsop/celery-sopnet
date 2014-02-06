547810c628aaa1be51ca6e2c906a92b4ef3a4dce
Tom Kazimiers <tom@voodoo-arts.net>
2014-02-06 16:34:19 -0500

    Add basic task definitions with dummy output

--- celerysopnet/tasks.py
+++ celerysopnet/tasks.py
@@ -8,3 +8,38 @@ def sample_task():
     This task is only used for testing and demonstration.
     """
     return "Sample task finished"
+
+@app.task
+def SliceGuarantorTask():
+    """
+    Calls SliceGuarantor for a certain block.
+    """
+    return "Called slice guarantor (dummy)"
+
+@app.task
+def SegmentGuarantorTask():
+    """
+    Calls SegmentGuarantor for a certain block.
+    """
+    return "Called segment guarantor (dummy)"
+
+@app.task
+def SolutionGuarantorTask():
+    """
+    Calls SolutionGuarantor for a certain core.
+    """
+    return "Called solution guarantor (dummy)"
+
+@app.task
+def SolveSubvolumeTask():
+    """
+    Calls SolutionGuarantorTask for all cores in a subvolume.
+    """
+    return "Called solution guarantor task for all cores (dummy)"
+
+@app.task
+def TraceNeuronTask():
+    """
+    Actively follows the cores of a neuron around a requested point.
+    """
+    return "Finished tracing neuron (dummy)"
