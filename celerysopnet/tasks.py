a7cbdf3e0fba5ad921432019bd508fc78f355f67
Tom Kazimiers <tom@voodoo-arts.net>
2014-02-10 16:06:04 -0500

    Remove sample task

--- celerysopnet/tasks.py
+++ celerysopnet/tasks.py
@@ -3,13 +3,6 @@ from __future__ import absolute_import
 from celerysopnet.celery import app
 
 @app.task
-def sample_task():
-    """
-    This task is only used for testing and demonstration.
-    """
-    return "Sample task finished"
-
-@app.task
 def SliceGuarantorTask():
     """
     Calls SliceGuarantor for a certain block.
