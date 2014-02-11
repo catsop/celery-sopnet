1b7973ee8e5da42609f890c819dc65b538a7ef60
Tom Kazimiers <tom@voodoo-arts.net>
2014-02-11 17:01:52 -0500

    Add simple segment guarantor precondition logic

--- celerysopnet/tasks.py
+++ celerysopnet/tasks.py
@@ -1,20 +1,55 @@
 from __future__ import absolute_import
 
+from celery import chord
+
 from celerysopnet.celery import app
 
+import pysopnet as ps
+
+from random import randint
+
+def call_segment_guarantor():
+    """
+    This is for now a mock-up which randomly returns required slices.
+    """
+    required_slices = [ps.point3(randint(0,10), randint(0,10), randint(0,10)) \
+            for i in range(randint(0,3))]
+
+    return required_slices
+
 @app.task
-def SliceGuarantorTask():
+def SliceGuarantorTask(x, y, z):
     """
-    Calls SliceGuarantor for a certain block.
+    Calls SliceGuarantor for a certain block. This task cannot fail (as long as
+    there is enough space for the results).
     """
-    return "Called slice guarantor (dummy)"
+    return "Created slice for (%s, %s, %s) (dummy)" % (x, y, z)
 
 @app.task
 def SegmentGuarantorTask():
     """
-    Calls SegmentGuarantor for a certain block.
+    Calls SegmentGuarantor for a certain block. If Sopnet returns with a
+    non-empty list, slices are missing for the segment to be generated. In this
+    case, this task will create a group of slice guarantor tasks for the missing
+    slices. It re-queues itself after them. It thereby makes sure all required
+    slices are available.
     """
-    return "Called segment guarantor (dummy)"
+    # Ask Sopnet for requested segment
+    required_slices = call_segment_guarantor()
+
+    # Fulfill preconditions, if any, before creating the segment
+    if required_slices:
+        # Create slice guarantor tasks for required slices
+        preconditions = [SliceGuarantorTask.s(rs.x, rs.y, rs.z) \
+              for rs in required_slices]
+        # Run a celery chain that re-executes the slice guarantor request after
+        # the preconditions are met.
+        callback = SegmentGuarantorTask.si()
+        result = chord(preconditions)(callback)
+        return "Queued %s new slice guarantor tasks and new segment " \
+                "guarantor task: %s (dummy)" % (len(preconditions), result.task_id)
+    else:
+        return "Created segment (dummy)"
 
 @app.task
 def SolutionGuarantorTask():
