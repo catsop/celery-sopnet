2d7d0ed286c463ba67dc0c69e6ec63de75877872
Tom Kazimiers <tom@voodoo-arts.net>
2014-06-19 19:56:35 -0400

    Reduce slice guarantor log level to 3

--- celerysopnet/tasks.py
+++ celerysopnet/tasks.py
@@ -12,7 +12,7 @@ def SliceGuarantorTask(config, x, y, z):
     Calls SliceGuarantor for a certain block. This task cannot fail (as long as
     there is enough space for the results).
     """
-    ps.setLogLevel(4)
+    ps.setLogLevel(3)
     x = int(x)
     y = int(y)
     z = int(z)
