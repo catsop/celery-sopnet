c32591a3a8f27676572b7d8b3ec727b0a806de51
Andrew Champion <andrew.champion@gmail.com>
2015-05-08 12:56:05 -0400

    Update for configuration stack descriptions

--- celerysopnet/tasks.py
+++ celerysopnet/tasks.py
@@ -115,8 +115,7 @@ def create_project_config(config):
 
         backend_type = [local|postgresql]
         catmaid_host = [Host:port of CATMAID]
-        catmaid_stack_ids = [dict of stack type to ID of the stack and
-            segmentation stack]
+        catmaid_stacks = [dict of stack type to dict description of the stack]
         catmaid_project_id = [ID of the CATMAID project in question]
         block_size = [3 element array for the block size in voxels]
         volume_size = [3 element array for the volume size in voxels]
@@ -136,14 +135,23 @@ def create_project_config(config):
     else:
         pc.setBackendType(ps.BackendType.Local)
 
-    for stack_type, ids in config['catmaid_stack_ids'].iteritems():
-        stack_ids = ps.StackIds()
-        stack_ids.id = ids['id']
-        stack_ids.segmentation_id = ids['segmentation_id']
+    for stack_type, stack_dict in config['catmaid_stacks'].iteritems():
+        stack = ps.StackDescription()
+        stack.imageBase = stack_dict['image_base'].encode('utf8')
+        stack.extension = stack_dict['file_extension'].encode('utf8')
+        stack.tileSourceType = stack_dict['tile_source_type']
+        stack.tileWidth = stack_dict['tile_width']
+        stack.tileHeight = stack_dict['tile_height']
+        stack.width = stack_dict['width']
+        stack.height = stack_dict['height']
+        stack.depth = stack_dict['depth']
+        stack.scale = stack_dict['scale']
+        stack.id = stack_dict['id']
+        stack.segmentationId = stack_dict['segmentation_id']
         if stack_type == 'Raw':
-            pc.setCatmaidStackIds(ps.StackType.Raw, stack_ids)
+            pc.setCatmaidStack(ps.StackType.Raw, stack)
         elif stack_type == 'Membrane':
-            pc.setCatmaidStackIds(ps.StackType.Membrane, stack_ids)
+            pc.setCatmaidStack(ps.StackType.Membrane, stack)
 
     block_size = config.get('block_size')
     if block_size:
@@ -161,7 +169,6 @@ def create_project_config(config):
     param_mapping = [
             {'name': 'catmaid_host', 'set': pc.setCatmaidHost, 'parse': str},
             {'name': 'catmaid_project_id', 'set': pc.setCatmaidProjectId, 'parse': int},
-            {'name': 'catmaid_stack_scale', 'set': pc.setCatmaidStackScale, 'parse': int},
             {'name': 'component_dir', 'set': pc.setComponentDirectory, 'parse': str},
             {'name': 'postgresql_host', 'set': pc.setPostgreSqlHost, 'parse': str},
             {'name': 'postgresql_port', 'set': pc.setPostgreSqlPort, 'parse': str},
